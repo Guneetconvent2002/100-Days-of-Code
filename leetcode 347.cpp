c6a732e4c4caf749c2e703b9576ea7ba3a1e1a86
Guneetconvent2002 <itsguneetsingh@gmail.com>
2023-05-25 23:51:02 +0530

    day 33

---
+++ leetcode 347.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int, int> freq;
+
+        for(auto num : nums) freq[num]++;
+
+        priority_queue<pair<int, int>> pq; // freq, key
+
+        for(auto f : freq) pq.push({f.second, f.first});
+
+        vector<int> ans;
+
+        while(k--){
+            ans.push_back(pq.top().second);
+            pq.pop();
+        }
+
+        return ans;
+    }
+};
